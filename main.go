e3f83481c1fa92bca62ca63bd3f3511557d51756
adtc2 <sectc2maranguape@gmail.com>
2022-05-19 21:37:07 -0300

    Ajustes no arquivo go.mod

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
